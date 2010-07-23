515354ff00457b1c0a9d6b4f461ceac528837dc3
Kevin Postal <kevin@2theleft.la>
2010-07-23 09:03:02 +0000

    added email login

--- main_site/views.py
+++ main_site/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
+# Logging In With Email Addresses in Django                
+# http://www.davidcramer.net/code/224/logging-in-with-email-addresses-in-django.html
+class EmailOrUsernameModelBackend(object):
+    def authenticate(self, username=None, password=None):
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+        try:
+            user = User.objects.get(**kwargs)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+ 
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+##########################################################################
 
 
 def index_view(request):
     #set up the dictonary
     context = {}
 
-
     return render_to_response('index.html',context,context_instance=RequestContext(request))
+
+def auth_view_login(request):
+    context = {}
+ 
+    if request.POST:
+        username = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        import pdb; pdb.set_trace()
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page.
+                return render_to_response('index.html',context,context_instance=RequestContext(request))        
+            else:
+                # Return a 'disabled account' error message
+                return render_to_response('index.html',context,context_instance=RequestContext(request))        
+        else:
+            # Return an 'invalid login' error message.
+                return render_to_response('index.html',context,context_instance=RequestContext(request))        
+
--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -1,38 +1,10 @@
-"""
-URL patterns for the views included in ``django.contrib.auth``.
-
-Including these URLs (via the ``include()`` directive) will set up the
-following patterns based at whatever URL prefix they are included
-under:
-
-* User login at ``login/``.
-
-* User logout at ``logout/``.
-
-* The two-step password change at ``password/change/`` and
-  ``password/change/done/``.
-
-* The four-step password reset at ``password/reset/``,
-  ``password/reset/confirm/``, ``password/reset/complete/`` and
-  ``password/reset/done/``.
-
-The default registration backend already has an ``include()`` for
-these URLs, so under the default setup it is not necessary to manually
-include these views. Other backends may or may not include them;
-consult a specific backend's documentation for details.
-
-"""
-
 from django.conf.urls.defaults import *
-
+from main_site.views import auth_view_login
 from django.contrib.auth import views as auth_views
-
 
 urlpatterns = patterns('',
                        url(r'^login/$',
-                           auth_views.login,
-                           {'template_name': 'registration/login.html'},
-                           name='auth_login'),
+                           auth_view_login),
                        url(r'^logout/$',
                            auth_views.logout,
                            {'template_name': 'registration/logout.html'},
