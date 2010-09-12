8b3259bff0b05162df63df9fb71e4f9ae3e4a847
Kevin Postal <kevindpostal@gmail.com>
2010-09-12 08:00:55 +0000

    even more..

--- basic/bookmarks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from basic.bookmarks.models import *
-
-
-class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ('url', 'description')
-    search_fields = ('url', 'description', 'extended')
-    prepopulated_fields = {'slug': ('title',)}
-admin.site.register(Bookmark, BookmarkAdmin)
--- basic/comments/templatetags/comments.py
+++
@@ -1,1 +0,0 @@
-from django.contrib.comments.templatetags.comments import *
